487e6e5e9c99106567ee12a84b01242d4a4ab09d
Rafael Estrada <restradap1600@gmail.com>
2021-03-08 23:21:14 -0600

    update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import Layout from '../components/Layout';
+import Head from 'next/head';
+import { Fragment } from 'react';
 
 const Index = () => {
     return (
-        <div>
-            <h1>Hola Mundo - NextJs</h1>
-        </div>
+        <Fragment>
+            <Head>
+                <title>Hi | Rafael Estrada</title>
+            </Head>
+            <Layout>
+                <h1>Rafael Estrada</h1>
+                <h2>MERN developer</h2>
+            </Layout>
+        </Fragment>
     );
 };
 
